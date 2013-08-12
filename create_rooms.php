dabb1740eb8a0cceb57362a0cbb646e4f3e316b7
shuvarek <shuvarek@gmail.com>
2013-08-12 16:42:04 +0100

    changed create_rooms.php
    every cell in table hes its own id: room_(x)_(y)
    used echo for simplisity where before was ?> and <?php

--- create_rooms.php
+++ create_rooms.php
@@ -1,17 +1,17 @@
 
 
 <table frame="border" cellpadding="0" cellspacing="0">
-<!-- should be changed just to <table class=...> in future -->
+<!-- should be changed just to <table class=...> in the future -->
 <?php
 $width =9;
 for( $i = 0; $i < $width; $i++)
 {
-	?><tr><?php
+	echo '<tr>';
 	for( $j = 0; $j < $width; $j++)
 	{
-		?><td><img src='pic/empty_room_16x16.png'></td><?php
+		echo "<td id='room_${j}_$i'>><img src='pic/empty_room_16x16.png'></td>";
 	}
-	?></tr><?php
+	echo "</tr>\n";
 }
 ?>
 
