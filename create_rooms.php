ac322fb324bb17e9ae47888e39828bbb29150380
shuvarek <shuvarek@gmail.com>
2013-08-12 16:20:22 +0100

    pulled from github which added README.md

---
+++ create_rooms.php
@@ -0,0 +1,18 @@
+
+
+<table frame="border" cellpadding="0" cellspacing="0">
+<!-- should be changed just to <table class=...> in future -->
+<?php
+$width =9;
+for( $i = 0; $i < $width; $i++)
+{
+	?><tr><?php
+	for( $j = 0; $j < $width; $j++)
+	{
+		?><td><img src='pic/empty_room_16x16.png'></td><?php
+	}
+	?></tr><?php
+}
+?>
+
+</table>
